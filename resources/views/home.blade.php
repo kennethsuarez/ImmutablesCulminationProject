c79245df6fd78769c454f651693ad75c583a3158
kennethsuarez <klsuarez1@up.edu.ph>
2021-02-05 00:54:48 +0800

    added authentication method

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-7">
+            <div class="card">
+                <div class="card-header">{{ __('Posts') }}</div>
+
+                <div class="card-body">
+                    Post Here
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
