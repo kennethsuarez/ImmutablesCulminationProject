a1ed6f9e7a4b8bd7d687129e4c7ca73a350637a4
Auxes1s <muignacio@up.edu.ph>
2021-02-06 11:26:23 +0800

    Changed html of profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,13 +3,17 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-3 p-5">
-                <img src="/dogpics/unnamed.png" height="200px" class="rounded-circle">
+            <div class="col-3 pr-5">
+                <img src="https://yt3.ggpht.com/ytc/AAUvwnhbv9F6Du9P6GVUYfap8qBfe2_nUGqH6xm3HE9C3Q=s900-c-k-c0x00ffffff-no-rj" height="150px" class="rounded-circle">
 
             </div>
             <div class="col-9 pt-5">
-                <div><h1>{{ $user->username }}</h1></div>
-                <a href="#">Add New Post</a>
+                <div class="d-flex justify-content-between pb-4 align-items-center">
+                    <div class="font-bold padding"><h1 style="font-size:30px">{{ $user->username }}</h1></div>
+                    <a href="#">Add New Post</a>
+                    <a href="#">Edit Profile</a>
+                </div>
+
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
@@ -23,13 +27,13 @@
 
         <div class="row pt-5">
             <div class="col-4">
-                <img src="/dogpics/rcas2ehoicv31.jpg" alt="" class="w-100">
+                <img src="https://pbs.twimg.com/media/ERO7sYjXUAAZVAi.jpg" alt="" class="w-100">
             </div>
             <div class="col-4">
-                <img src="/dogpics/EQa4IfPX0AAMity.jpg" alt="" class="w-100">
+                <img src="https://pbs.twimg.com/media/EbyQ51DXgAcA_gs.jpg" alt="" class="w-100">
             </div>
             <div class="col-4">
-                <img src="/dogpics/EktLH4tWkAA_UQ-.jpg" alt="" class="w-100">
+                <img src="https://sbly-web-prod-shareably.netdna-ssl.com/wp-content/uploads/2020/06/03165105/tucker-1.jpg" alt="" class="w-100">
             </div>
         </div>
     </div>
