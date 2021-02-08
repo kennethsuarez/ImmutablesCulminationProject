316ddc7112fda615a584e1c6516294d9314f2b2b
kennethsuarez <klsuarez1@up.edu.ph>
2021-02-08 16:37:45 +0800

    added users in explore page

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        @foreach ($users as $user)
+    @foreach($users as $user)
+    <div class="flex items-center mb-5">
+        <!-- @foreach ($users as $user)
             <img src="{{$user->avatar}}"
                 alt="{{$user->username}}"
                 width="60"
             >
-        @endforeach
+        @endforeach -->
+
+        <div>
+            <h4 class="font-bold">{{ '@' . $user->id}}</h4>
+        </div>
     </div>
+    @endforeach
 @endsection
