f4180dd97e096059a9919b5956e71a5761846495
kennethsuarez <klsuarez1@up.edu.ph>
2021-02-08 14:57:01 +0800

    fixed code for following-list.blade.php

--- resources/views/following-list.blade.php
+++ resources/views/following-list.blade.php
@@ -3,15 +3,15 @@
     @foreach (auth()->user()->follows as $user)
     <li class="mb-2">
         <div class="flex items-center text-sm">
-            <a href="/"
-            <img 
-                src="{{ $user->avatar }}"
-                alt=""
-                class="rounded-full mr-2"
-            >
+            <a href="/">
+                <img
+                    src="{{ $user->avatar }}"
+                    alt=""
+                    class="rounded-full mr-2"
+                >
 
             {{ $user->name }}
-            >
+            </a>
         </div>
     </li>
     @endforeach
