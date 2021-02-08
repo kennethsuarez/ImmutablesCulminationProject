d4baa80cbfc0ab405248f9269a861da79b221f07
Auxes1s <muignacio@up.edu.ph>
2021-02-09 00:03:06 +0800

    cosmetic changes, moved profile image to top

--- resources/views/publish-panel.blade.php
+++ resources/views/publish-panel.blade.php
@@ -1,23 +1,19 @@
 <div class="border border-blue rounded-lg py-6 px-8 mb-8">
     <form action="posts" method="post">
         @csrf
-        <textarea
-            name="body"
-            class="w-full"
-            placeholder="What's on your mind?"
-            required
-        ></textarea>
+        <div class="d-flex justify-content-between">
+            <textarea
+                name="body"
+                class="w-full"
+                placeholder="What's on your mind?"
+                required
+            ></textarea>
 
-        <hr class="my-4">
-
-        <footer class="flex justify-between">
-            <img
-            src="{{ auth()->user()->profile->profileImage() }}"
-            alt=""
-            class="rounded-full mr-2 w-16"
-            >
+            <hr class="my-4">
 
             <button type="submit" class="bg-blue-500 shadow rounded-lg py-2 px-2 text-white">Post</button>
-        </footer>
+        </div>
+
+
     </form>
 </div>
