8e0a9109087231256286e1e293e2d55bc7c79f27
Auxes1s <muignacio@up.edu.ph>
2021-02-08 23:42:54 +0800

    Changed the link of the website logo to direct to /home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     <div><img src="/svg/Tweetylogo.svg" style="max-height: 40px"></div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
