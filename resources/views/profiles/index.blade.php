43281b21bb5129a54cd06fc60c75de391df84cee
Auxes1s <muignacio@up.edu.ph>
2021-02-08 22:06:59 +0800

    Reset

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -1,35 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="pb-4 container">
+        <div class="pb-4 row">
             <div class="col-3 pr-5">
-                
+
                 <img src="{{ asset('storage/'.$user->profile->image) }}" height="150px" class="rounded-circle">
 
             </div>
             <div class="col-9 pt-5">
                 <div class="d-flex justify-content-between pb-3 align-items-center">
                     <div class="font-bold"><h1 style="font-size:30px">{{ $user->username }}</h1></div>
-
                     @can('update', $user->profile)
-                        <a href="#">Add New Post</a>
+                        <a
+                            class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2"
+                            href="/profile/{{ $user->id }}/edit"
+                        >Edit Profile</a>
                     @endcan
+                    {{--@can('update', $user->profile)
+                        <a href="#">Add New Post</a>
+                    @endcan--}}
                 </div>
 
-                <div class="flex mb-2"> 
-                    @can('update', $user->profile)
-                    <a 
-                        class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2" 
-                        href="/profile/{{ $user->id }}/edit"
-                    >Edit Profile</a>
-                    @endcan
+                <div class="flex mb-2">
 
                     @if(auth()->check() && auth()->user()->id !== $user->id)
                         <form method="POST" action="/profile/{{$user->id}}/follow">
                             @csrf
                             <button type="submit"
-                                class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs" 
+                                    class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
                             >
                                 {{ auth()->user()->following($user)? 'Unfollow' : 'Follow'}}
                             </button>
@@ -37,14 +36,14 @@
                     @endif
                 </div>
 
-                <div class="d-flex mb-6">
+                <div class="d-flex mb-4">
                     <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> {{Str::plural('post', $user->posts->count())}}</div>
                     <div class="pr-5"><strong>{{ $user->follows->count() }}</strong> followers</div>
                     <div class="pr-5"><strong>{{ $user->follows->count() }}</strong> following</div>
                 </div>
-                <!-- <div class="pt-4 font-weight-bold"> {{ $user->profile->title }} </div>
+                <div class="font-weight-bold"> {{ $user->profile->title }} </div>
                 <div>{{ $user->profile->description }}</div>
-                <div><a href="#">{{ $user->profile->url }}</a></div> -->
+                <div><a href="#">{{ $user->profile->url }}</a></div>
             </div>
         </div>
 
